c0677bd55c29d0b469d239ec335c0b7412a3d3d2
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2025-03-20 02:34:40 +0500

    first commit

---
+++ Assignments_00_to_05/add_two_numbers/app.py
@@ -0,0 +1,8 @@
+def main():
+ num1:int = int(input("Enter the first number: "))
+ num2:int = int(input("Enter the second number: "))
+ sum:int  = num1 + num2
+ print(f"Sum of two numbers are: {sum}") 
+
+if __name__ == '__main__':
+    main()
