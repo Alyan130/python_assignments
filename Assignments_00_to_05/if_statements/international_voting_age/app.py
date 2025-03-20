e9906e5e56572e5cfc905f6bb1998c3914226556
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2025-03-20 17:42:33 +0500

    voting

---
+++ Assignments_00_to_05/if_statements/international_voting_age/app.py
@@ -0,0 +1,24 @@
+PETURKSBOUIPO_AGE: int = 16
+STANLAU_AGE: int = 25
+MAYENGUA_AGE: int = 48
+
+def main():
+    age: int = int(input("Enter your age: "))
+
+    if age >= PETURKSBOUIPO_AGE:
+        print(f"You can vote in Peturksbouipo where the voting age is {PETURKSBOUIPO_AGE}.")
+    else:
+        print(f"You cannot vote in Peturksbouipo where the voting age is {PETURKSBOUIPO_AGE}.")
+    
+    if age >= STANLAU_AGE:
+        print(f"You can vote in Stanlau where the voting age is {STANLAU_AGE}.")
+    else:
+        print(f"You cannot vote in Stanlau where the voting age is {STANLAU_AGE}.")
+    
+    if age >= MAYENGUA_AGE:
+        print(f"You can vote in Mayengua where the voting age is {MAYENGUA_AGE}.")
+    else:
+        print(f"You cannot vote in Mayengua where the voting age is {MAYENGUA_AGE}.")
+
+if __name__ == '__main__':
+    main()
