7c5d67f688d8363c42e1c395e125287f1d798411
Alyan130 <159697584+Alyan130@users.noreply.github.com>
2025-03-25 03:06:51 +0500

    Madlibs game

---
+++ Assignments_01_to_06/mad_libs/app.py
@@ -0,0 +1,10 @@
+
+print("Welcome to MADLIBS game!✨")
+
+adjective1=input("Enter an adjective : ")
+noun=input("Enter an noun : ")
+place=input("Enter an place : ")
+adjective2=input("Enter an adjective : ")
+
+
+print(f"Yesterday, I saw a {adjective1} {noun} dancing on the {place}, and it was the most {adjective2} thing ever!")
